4f1288f6b74fb725b0e8b7b3a65393ef341c2cb0
Michael Kim <michael.kim.200518@gmail.com>
2021-05-18 17:15:22 +0900

    Add multi nodes

--- drm.sh
+++ drm.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-rm -rf agora/.cache/*
+rm -rf agora/node/0/.cache/*
+rm -rf agora/node/2/.cache/*
+rm -rf agora/node/3/.cache/*
+rm -rf agora/node/4/.cache/*
+rm -rf agora/node/5/.cache/*
+rm -rf agora/node/6/.cache/*
+rm -rf agora/node/7/.cache/*
 rm -rf stoa/data
 rm -rf stoa/logs
 rm -rf sendtx/logs
