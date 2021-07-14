31b3db3c107014ce1fcfa5893be047da32d9a47a
Michael Kim <michael.kim.200518@gmail.com>
2021-07-14 15:15:10 +0900

    Update

--- drm.sh
+++ drm.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-rm -rf agora/node/0/.cache
 rm -rf agora/node/2/.cache
 rm -rf agora/node/3/.cache
 rm -rf agora/node/4/.cache
 rm -rf agora/node/5/.cache
 rm -rf agora/node/6/.cache
-rm -rf agora/node/7/.cache
 rm -rf agora/.cache
 rm -rf agora/log
 rm -rf stoa/data
